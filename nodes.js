a9fc126d98fb8ab419ef4bf7d064758199c87a83
Th0se <thomasap1464@gmail.com>
2023-03-13 14:35:50 +0700

    1. Add a function to delete nodes from a tree. 2. Add a function to return an array containing every node's value. 3. Make the code more self-documenting overall.

--- nodes.js
+++ nodes.js
@@ -1,5 +1,7 @@
 const Node = (rootValue, leftArray, rightArray) => {
-    // A factory to make a node 
+    /* A factory to make a node for a binary search tree.
+        This is to be used in functions that build a binary search tree.
+    */
     const result = {
         data: rootValue,
         left: leftArray,
@@ -46,7 +48,7 @@ const mergeSort = (array) => {
 };
 
 const removeDuplicate = (array) => {
-    // A function to remove duplicates.
+    // A function to remove duplicates from an array.
     const encountered = new Set(array);
     const result = [...encountered];
     return result;
@@ -59,15 +61,15 @@ const buildTree = (array) => {
     } else if (array.length === 0) {
         return null;
     } else {
-        const strippedArray = removeDuplicate(array);
+        const uniqueValues = removeDuplicate(array);
         // Remove duplicates in the array.
-        const sortedArray = mergeSort(strippedArray);
-        const middlePoint = Math.floor(sortedArray.length / 2);
-        const left = sortedArray.slice(0, middlePoint);
-        const right = sortedArray.slice(middlePoint+1, sortedArray.length);
+        const sortedUniqueValues = mergeSort(uniqueValues);
+        const middlePoint = Math.floor(sortedUniqueValues.length / 2);
+        const left = sortedUniqueValues.slice(0, middlePoint);
+        const right = sortedUniqueValues.slice(middlePoint+1, sortedUniqueValues.length);
         const leftBranch = buildTree(left);
         const rightBranch = buildTree(right);
-        const result = Node(sortedArray[middlePoint], leftBranch, rightBranch)
+        const result = Node(sortedUniqueValues[middlePoint], leftBranch, rightBranch)
         return result;
     };
 };
@@ -83,26 +85,87 @@ const Tree = (array) => {
 };
 
 const insert = (number, node) => {
-    // A function to insert a number into a tree
+    // A function to insert a number into a tree.
+    /*  This is called on the root of a binary search tree,
+        not the tree itself.
+    */
     if (number === node.data) {
         return;
     } else if (number > node.data) {
         if (node.right === null) {
-            node.right = Object(node.right);
-            node.right.data = number;
-            node.right.right = null;
-            node.right.left = null;
+            /* Change node.right from null to a node,
+                then assign the number into it.
+            */
+            node.right = Node(number, null, null);
         } else {
             insert(number, node.right)
         };
     } else if (number < node.data) {
         if (node.left === null) {
-            node.left = Object(node.left);
-            node.left.data = number;
-            node.left.left = null;
-            node.left.right = null;
+            /* Change node.left from a null to a node,
+                then assign the number into it.
+            */
+            node.left = Node(number, null, null);
         } else {
             insert(number, node.left)
         };
+    };
+};
+
+const remove = (number, node, parent) => {
+    // A function to remove a number from a tree.
+    /*  The parameter `parent` is not needed when calling the function.
+        That parameter is only used when recursing.
+    */
+    /*  This function is called to the root of a binary search tree,
+        not the tree itself.
+    */
+    if (number === node.data) {
+        if (node.right === null) {
+            if (node.left === null) {
+                if (parent.left === node) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                };
+            } else {
+                node.data = node.left.data;
+                node.left = node.left.left;
+            };
+        } else {
+            node.data = node.right.data;
+            if (!node.right.right === null) {
+                node.right = node.right.right
+            } else {
+                node.right = node.right.left;
+            };
+        };
+    } else if (number <= node.data) {
+        remove(number, node.left, node);
+    } else if (number >= node.data) {
+        remove(number, node.right, node);
+    };
+};
+
+const levelOrder = (node, result = [], queue = []) => {
+    /* A function to return an array containing the value
+        of every node in a binary search tree. 
+    */
+    /*  This is called on the root of the tree, not the tree itself.
+        The parameters `result` and `queue` are exclussively for recursing.
+    */
+    if (node === null) {
+        return;
+    } else {
+        result.push(node.data);
+        queue.push(node.left);
+        queue.push(node.right);
+        while (queue.length) {
+            const currentLevel = queue[0];
+            queue.shift();
+            levelOrder(currentLevel, result, queue);
+        };
+
+        return result;
     };
 };
